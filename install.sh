9e1f897d8f5def046027553bb757d13bce7e99dc
Ivo Woltring <Ivo@IvoNet.nl>
2018-05-23 23:45:12 +0200

    small changes

--- install.sh
+++ install.sh
@@ -39,7 +39,7 @@ node_build() {
     clean_state $2
 }
 
-node_build 6.14.2-alpine node6
+#node_build 6.14.2-alpine node6
 node_build 8.11.2-alpine node8
 node_build 9.11.1-alpine node9
 node_build 10.1.0-alpine node10
